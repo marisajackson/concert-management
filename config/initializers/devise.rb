2367a2c3d744522f89b51547bae5b6a52b67af47
MaRisa Jackson <mhljackson@gmail.com>
2014-09-16 11:44:14 -0500

    Add figaro

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,7 +272,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, "1519870068250808", "855cf14bbbd3edb3f09e86f66fa08195"
+  config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET_KEY"]
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
