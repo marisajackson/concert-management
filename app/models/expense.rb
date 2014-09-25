31361ff2f6ab0269514c7dc6504e05ee41bd3dfb
MaRisa Jackson <mhljackson@gmail.com>
2014-09-25 11:25:48 -0500

    Add differnce column to finanaces

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,14 @@
 class Expense < ActiveRecord::Base
   belongs_to :concert
   belongs_to :expense_category
+  attr_accessor :difference
+
+  def difference
+    if self.actual_cost.nil?
+      actual = 0
+    else
+      actual = self.actual_cost
+    end
+    return self.expected_cost - actual
+  end
 end
--- app/models/income.rb
+++ app/models/income.rb
@@ -1,4 +1,14 @@
 class Income < ActiveRecord::Base
   belongs_to :concert
   belongs_to :income_category
+  attr_accessor :difference
+
+  def difference
+    if self.actual_cost.nil?
+      actual = 0
+    else
+      actual = self.actual_cost
+    end
+    return self.expected_revenue - actual
+  end
 end
